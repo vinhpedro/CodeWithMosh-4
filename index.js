e3fc67033d579704ce3260544c4fb99e95802f71
Aleksandr Bartov <aleksandrbartov@yandex.ru>
2018-03-28 00:06:25 +0400

    have learnd to post, put, get anв delete items

--- index.js
+++ index.js
@@ -12,40 +12,67 @@ const courses = [
   { id: 3, name: 'course3'},
 ];
 
+
 app.get('/', (req, res) => {
   res.send('hello, Sasha!!!');
 });
+
 
 app.get('/api/courses', (req, res) => {
   res.send(courses);
 });
 
+
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  const result = Joi.validate(req.body, schema);
-
-  if (result.error) {
-    res.status(404).send(result.error.details[0].message);
-    return;
-  }
+  const {error} = validateCourse(req.body); //result.error
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
     name: req.body.name
   };
-
   courses.push(course);
   res.send(course);
 });
 
+
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('the page is not found');
+  if (!course) return res.status(404).send('the page is not found');
   res.send(course);
 });
+
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('the page is not found');
+
+  const {error} = validateCourse(req.body); //result.error
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('the page is not found');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
+
+
+
+function validateCourse (course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return Joi.validate(course, schema);
+}
+
 
 
 const port = process.env.PORT || 3000;
