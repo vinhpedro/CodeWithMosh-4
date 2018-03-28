30254a94d9986af3b3db35606e74848f1714cbe2
Aleksandr Bartov <aleksandrbartov@yandex.ru>
2018-03-28 21:20:53 +0400

    helmet, morgan npm installing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
 const logger = require('./logger');
 const express = require('express');
@@ -7,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
 
 
 app.use(logger);
