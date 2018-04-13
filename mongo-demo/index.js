04b900716689e46da03835f4dc70a7db95196c18
Aleksandr <aleksandrbartov@yandex.ru>
2018-04-14 03:26:09 +0400

    working with Moth for MongoDB

---
+++ mongo-demo/index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [ String ],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: 'Css course',
+    author: 'Sasha Bartov',
+    tags: ['css', 'frontend'],
+    isPublished: true
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function getCourses() {
+  const courses = await Course
+    .find({ author: 'Sasha Bartov'})
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+
+getCourses();
